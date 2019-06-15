27189eeef267d7fc69d26043e4be21d5b3e4d03d
Ankush Yadav <ankushyadav9302@gmail.com>
2019-06-15 12:14:36 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <div id="header">
   <div id="header-content">
     <div class="logo"><a href="index.php"><img src="images/icons/logo.png" style="height: 50px; width: 50px"></a></div>
-    <div class="logo-text">National Institue of Technology, Sikkim</div>
+    <div class="logo-text">Title</div>
     <div class="header-item" style="margin-right: 50px"><a href="signup.php"><i class="material-icons header-icon">person_add</i>Signup</a></div>
     <div class="header-item open"><a  href="javascript:void(0)" style="cursor: pointer" onclick="javascript:login_pop()"><i class="material-icons header-icon">exit_to_app</i>Login</a></div>
     <div class="header-item"><a href="javascript:menu()"><i class="material-icons header-icon">menu</i></a></div>
