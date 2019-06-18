3d2c93adc7ec80ddb5a8b7cec84c5c62ea630092
Gourav Kumar Prasad <gouravprasad@gmail.com>
2019-06-18 21:13:30 +0530

    footer copied  from web

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
     <title>Students Website</title>
     
     <link rel="stylesheet" href="css/custom.css">
-    <link rel="stylesheet" href="fonts/fonts.css">    
+    <link rel="stylesheet" href="fonts/fonts.css">  
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Footer-with-button-logo.css">
   </head>
 
   <body style="font-family: 'Google Sans'" class="fixed-background">
@@ -48,13 +52,56 @@
       </div>
 
     </section>
-    <footer>
-      
+    <footer id="myFooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h2 class="logo"><a href="#"> LOGO </a></h2>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Links</h5>
+                    <ul>
+                        <li><a href="https://www.nitsikkim.ac.in" target="_blank">NIT Sikkim</a></li>
+                        <li><a href="https://www.placement.nitsikkim.ac.in" target="_blank">TnP Cell</a></li>
+                        <li><a href="https://www.kic.nitsikkim.ac.in" target="_blank">KIC Library</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>About us</h5>
+                    <ul>
+                        <li><a href="#">Company Information</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Reviews</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Support</h5>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Help desk</a></li>
+                        <li><a href="#">Forums</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <div class="social-networks">
+                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+                    </div>
+                    <button type="button" class="btn btn-default">Contact us</button>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2019 Copyright | NIT Sikkim </p>
+        </div>
     </footer>
 
     <script src="js/jquery-3.3.1.js"></script>
     <script src="js/validate.min.js"></script>
     <script src="js/custom.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
   </body>
 </html>
