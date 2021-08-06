5bf70cfd9b186e0e11f446f17d0b808ccd352818
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-08-06 19:37:21 +0530

    creating skeleton

---
+++ src/components/Explore.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  main
+    div
+      div
+        ul
+          li Search Resources
+    div
+      div
+        form(method="post")#new-search
+
+</template>
+
+<script>
+export default {
+  name: "Explore"
+}
+</script>
+
+<style scoped>
+
+</style>
