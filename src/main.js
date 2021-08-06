c4e059d2e91811c459bbcf7c974cbfe3331cd6ac
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-08-06 20:20:03 +0530

    add firebase configs

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/utils/firebase/firebase-init'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
