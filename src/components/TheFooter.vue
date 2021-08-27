2cc08ff28e98bacd56b960779c5f7a776d16aed5
Neelava Chatterjee <neelavac53@gmail.com>
2021-08-27 19:04:23 +0530

    Make footer stick to the bottom

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-        <p>Made with <b-icon icon='suit-heart-fill' variant='danger'></b-icon></p>
-    </div>
+  <div class="footer fixed-bottom">
+    <p>Made with <b-icon icon='suit-heart-fill' variant='danger'></b-icon></p>
+  </div>
 </template>
 
 <script>
-export default {};
+  export default {};
 </script>
